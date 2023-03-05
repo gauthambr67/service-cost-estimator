59a60117037a129a78cc2b90f4d9518d7eb08ee7
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-03-05 02:13:24 -0500

    created components

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import "./NewOrderPage.css";
 import { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-function NewOrderPage() {
+function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  const categoriesRef = useRef([]);
+
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set(items.map(item => item.category.name))];
+      setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
+    }
+    getItems();
+  }, []);
+  // An empty dependency array results in the effect
+  // function running ONLY after the FIRST render
+  
   return (
     <main className="NewOrderPage">
-      <h1>NewOrderPage</h1>
-      <button onClick={setMenuItems}>Trigger re-render</button>
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
     </main>
   );
 }
