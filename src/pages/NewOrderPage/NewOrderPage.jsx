0dfd163b00d23f3b9e91d68f57df5dc03a51f438
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-03-05 01:44:51 -0500

    added seed.js

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./NewOrderPage.css";
+import { useState, useEffect} from 'react';
 
 function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
   return (
     <main className="NewOrderPage">
       <h1>NewOrderPage</h1>
+      <button onClick={setMenuItems}>Trigger re-render</button>
     </main>
   );
 }
