30925a9d252802190d6e2322764f6c089d3a70d8
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-03-06 15:03:42 -0500

    updating App file

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import "./App.css";
 import { getUser } from "../../utilities/users-service";
 
-function App() {
+export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
-      { user ?
-        <Routes>
-          <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
-          <Route path="/orders" element={<OrderHistoryPage />} />
-        </Routes>
-        :
+      {user ? (
+        <>
+          <Routes>
+            <Route
+              path="/orders/new"
+              element={<NewOrderPage user={user} setUser={setUser} />}
+            />
+            <Route
+              path="/orders"
+              element={<OrderHistoryPage user={user} setUser={setUser} />}
+            />
+            <Route path="/*" element={<Navigate to="/orders/new" />} />
+          </Routes>
+        </>
+      ) : (
         <AuthPage setUser={setUser} />
-      }
+      )}
     </main>
   );
 }
-
-export default App;
