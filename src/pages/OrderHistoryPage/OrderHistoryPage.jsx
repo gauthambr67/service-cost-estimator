e838bf60a545ec28aecf620f6e8aa2c8cb607c6b
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-03-06 15:29:10 -0500

    updating NewOrderPage and OrderHistory Page

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import * as ordersAPI from "../../utilities/orders-api";
 import "./OrderHistoryPage.css";
 import { checkToken } from "../../utilities/users-service";
+import Logo from "../../components/Logo/Logo";
+import UserLogOut from "../../components/UserLogOut/UserLogOut";
+import OrderDetail from "../../components/OrderDetail/OrderDetail";
+import OrderList from "../../components/OrderDetail/OrderList";
 
-function OrderHistoryPage() {
-  async function handleCheckToken() {
-    let expDate = await checkToken();
-    console.log(expDate);
-  }
+export default function OrderHistoryPage({ user, setUser }) {
+  const [orders, setOrders] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+
+  useEffect(function () {
+    async function getOrders() {
+      const orders = await ordersAPI.getAllForUser();
+      setOrders(orders);
+      setSelectedOrder(orders[0]);
+    }
+    getOrders();
+  }, []);
+
   return (
     <main className="OrderHistoryPage">
-      <h1>OrderHistoryPage</h1>
-      <button onClick={handleCheckToken}>Check when login expires</button>
+      <aside>
+        <Logo />
+        <Link to="/orders/new" className="button btn-sm">
+          NEW ORDER
+        </Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+
+      <OrderList
+        orders={orders}
+        selectedOrder={selectedOrder}
+        setSelectedOrder={setSelectedOrder}
+      />
+
+      <OrderDetail order={selectedOrder} />
     </main>
   );
 }
-
-export default OrderHistoryPage;
